07168d5b3df76d99618f093c6db1db920ea8a381
NikolajMakarovskij <adrameleh666@gmail.com>
2023-11-23 13:55:59 +0500

    update filter

--- backend/pages/Filtred_data.py
+++ backend/pages/Filtred_data.py
@@ -14,17 +14,29 @@ class FiltredDataView(View, DataMixin):
         column_name = ['number', 'date']
         data = cls.data_filling(data, column_name)
         st.subheader('Текущий год, станция 2')
-        filtered_data = (data['date'] >= cur_year.strftime('%Y')) & (data['station'] == 'two')
+        filtered_data = (
+                                data['date'] >= f'{int(cur_year.strftime('%Y'))-1}'
+                        ) & (
+                                data['date'] <= f'{int(cur_year.strftime('%Y'))}'
+                        ) & (
+                                data['station'] == 'two'
+                        )
         st_two = cls.data_filtered(data, filtered_data)
         st.write(st_two)
         st.subheader('Текущий год, станция 3')
-        filtered_data = (data['date'] >= cur_year.strftime('%Y')) & (data['station'] == 'three')
+        filtered_data = (
+                                data['date'] >= f'{int(cur_year.strftime('%Y'))-1}'
+                        ) & (
+                                data['date'] <= f'{int(cur_year.strftime('%Y'))}'
+                        ) & (
+                                data['station'] == 'three'
+                        )
         st_three = cls.data_filtered(data, filtered_data)
         st.write(st_three)
         column_name = 'quantity'  # При передаче строки возвращает str. При передаче списка возвращает dict
         sum_st_two = cls.get_column_sum(st_two, column_name)
         sum_st_three = cls.get_column_sum(st_three, column_name)
-        output_data = pd.DataFrame([{'station': 'two', '2023': sum_st_two}, {'station': 'three', '2023': sum_st_three}])
+        output_data = pd.DataFrame([{'station': 'two', '2022': sum_st_two}, {'station': 'three', '2022': sum_st_three}])
         st.subheader('Количество пассажиров')
         st.write(output_data)
 
